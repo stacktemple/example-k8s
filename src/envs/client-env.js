ea6617f5e604d7b4cf7ea0f55e26dfaedae203eb
Thitiphum <stacktemple@gmail.com>
2025-04-23 18:37:19 +0700

    add comment explaining client-env.js usage

--- src/envs/client-env.js
+++ src/envs/client-env.js
@@ -3,5 +3,5 @@ window.env = {
   VITE_BASE_URL: "http://localhost:4000",
 
   // can be
-  //VITE_BASE_URL: "http://{{ ip-address }}:4000" or some domain
+  //VITE_BASE_URL: "http://{{ ip-address }}:4000" or any domain
 };
